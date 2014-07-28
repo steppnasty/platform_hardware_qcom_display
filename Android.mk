0c23ebe2f85155f6c66bf8206beecea348077554
steppnasty <steppnasty@gmail.com>
2014-07-28 10:09:23 -0400

    Disable building of liblight

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libqservice
 
 include $(call all-named-subdir-makefiles,$(display-hals))
