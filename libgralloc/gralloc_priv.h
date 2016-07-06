87ea0df7e5c21894d6dfd789b3c19263f3e9d530
steppnasty <steppnasty@gmail.com>
2016-07-06 15:28:44 -0400

    libgralloc: Add camera fixes

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -70,7 +70,7 @@ enum {
      * which can be used for buffers that are not secured for DRM
      * but still need to be protected from screen captures
      */
-    GRALLOC_USAGE_PRIVATE_CP_BUFFER       =       0x00400000,
+    GRALLOC_USAGE_PRIVATE_CAMERA_HEAP       =       0x00400000,
 };
 
 enum {
